f89ec9e34b8eab776ae4fe55dd9683d38782feb7
BuildTools <unconfigured@null.spigotmc.org>
2022-08-31 22:33:31 +0200

    Add demo js

--- src/js/demo.js
+++ src/js/demo.js
@@ -1,16 +1,27 @@
 const ctx = $("#demoChart");
-const demoTable = $('.chart__demo-table')
+const demoTable = $(".chart__demo-table");
 
-const data = [12, 19, 3, 5, 2, 3];
-const labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
-const colors = [
-	"rgba(255, 99, 132, 1)",
-	"rgba(54, 162, 235, 1)",
-	"rgba(255, 206, 86, 1)",
-	"rgba(75, 192, 192, 1)",
-	"rgba(153, 102, 255, 1)",
-	"rgba(255, 159, 64, 1)",
-];
+let data = [];
+let labels = [];
+let colors = [];
+$(".chart__demo-values")
+	.children()
+	.toArray()
+	.map((item) => {
+		data.push(item.innerHTML);
+	});
+$(".chart__demo-labels")
+	.children()
+	.toArray()
+	.map((item) => {
+		labels.push(item.innerHTML);
+	});
+$(".chart__demo-colors")
+	.children()
+	.toArray()
+	.map((item) => {
+		colors.push(item.getAttribute('data-color'))
+	});
 
 const myChart = new Chart(ctx, {
 	type: "bar",
